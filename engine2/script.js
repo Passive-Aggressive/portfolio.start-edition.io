b39ee3375e8254b9a7393b82804ed24a19a39ed5
Passive-Aggressive <41363880+Passive-Aggressive@users.noreply.github.com>
2019-01-24 11:21:57 +0300

    rework slider2

--- engine2/script.js
+++ engine2/script.js
@@ -18,4 +18,4 @@ jQuery.extend(jQuery.easing,{easeInOutQuart:function(e,f,a,h,g){if((f/=g/2)<1){r
 // Obfuscated by Javascript Obfuscator
 // http://javascript-source.com
 //***********************************************
-jQuery("#wowslider-container2").wowSlider({effect:"carousel",prev:"",next:"",duration:20*100,delay:20*100,width:770,height:482,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:true,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
+jQuery("#wowslider-container2").wowSlider({effect:"carousel",prev:"",next:"",duration:20*100,delay:20*100,width:770,height:482,autoPlay:true,autoPlayVideo:false,playPause:false,stopOnHover:false,loop:false,bullets:0,caption:false,captionEffect:"parallax",controls:false,controlsThumb:false,responsive:1,fullScreen:false,gestures:2,onBeforeStep:0,images:0});
